77483db9672d3de4a339f21e62e675732d920ff4
jamesuidev <jamesbaby774@gmail.com>
2022-05-24 17:18:37 +0530

    created modules for fetch and generartepictures

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const auth = "563492ad6f9170000100000183786d49667b4e96afccc9318e3056af";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
 
-async function curatedPhotos() {
-  const dataFetch = await fetch("https://api.pexels.com/v1/curated", {
+// Add event listeners
+searchInput.addEventListener("input", updateInput);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+// creating nodules for fetching and generate
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
     method: "GET",
     headers: {
       Accept: "application/json",
@@ -13,15 +25,40 @@ async function curatedPhotos() {
     },
   });
   const data = await dataFetch.json();
-  //   data parsed into object that we can make use of
+  return data;
+}
+function generatePictures(data) {
   data.photos.forEach((photo) => {
-    console.log(photo);
     const galleryImg = document.createElement("div");
     galleryImg.classList.add(".gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-    <p>${photo.photographer}</p>`;
+    galleryImg.innerHTML = `
+    <div class="gallery-info">
+    <p>${photo.photographer}</p>
+    <a href=${photo.src.original} >Download</a>
+    </div>
+    <img src=${photo.src.large}></img>`;
     gallery.appendChild(galleryImg);
   });
 }
-
+async function curatedPhotos() {
+  const data = await fetchApi(`
+    https://api.pexels.com/v1/curated?per_page=15&page=1
+  `);
+  generatePictures(data);
+  //   data parsed into object that we can make use of
+}
+async function searchPhotos(query) {
+  clear();
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1
+  `
+  );
+  //   data parsed into object that we can make use of
+  generatePictures(data);
+}
+// for clearing the loaded image and typed word after clicking search
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
 curatedPhotos();
